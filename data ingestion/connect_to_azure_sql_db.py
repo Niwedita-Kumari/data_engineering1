15f1bb6c686e1f693dd20dc4ec8d33f6f9b7bef9
Niwedita-Kumari <niwedita78@gmail.com>
2025-12-02 12:35:23 +0530

    Create connect_to_azure_sql_db.py

---
+++ data ingestion/connect_to_azure_sql_db.py
@@ -0,0 +1,10 @@
+%python
+
+df = spark.read.format("jdbc") \
+    .option("url", "jdbc:sqlserver://{servername:portname};databaseName={databasename};") \
+    .option("dbtable", "{table_name}") \
+    .option("user", "{username}") \
+    .option("password", "{password}") \
+    .load()
+
+df.show()
