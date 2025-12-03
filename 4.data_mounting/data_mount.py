682c3ebe7d1bbb17e4bef675a6011746b2acaf6b
Niwedita-Kumari <niwedita78@gmail.com>
2025-12-03 07:48:06 +0530

    Update data_mount.py

--- 4.data_mounting/data_mount.py
+++ 4.data_mounting/data_mount.py
@@ -1,1 +1,9 @@
+# command to mount the BLOB storage and save in the mount location
 
+dbutils.fs.mount(
+  source = "wasbs://{container}@{storage}.blob.core.windows.net",
+  mount_point = "{mount_path}",
+  extra_configs = {"fs.azure.account.key.{storage}.blob.core.windows.net": "{access_key}"}
+)
+
+df.write.options(header='True').csv("{mount_path}")
